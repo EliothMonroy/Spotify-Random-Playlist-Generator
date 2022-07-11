ccc907d0fd1eb079544699a3c672437205659f17
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2022-07-11 17:57:29 -0500

    Update script for 2022

--- readme.md
+++ readme.md
@@ -6,22 +6,22 @@ Requirements:
 
 Steps-to-follow:
 
-1.- First at all, you need to download the 'test.py' file. 
+1.- First you need to download the 'test.py' file. 
 
-2.- You have to create a playlist in Spotify with at least one track inside and then obtain their playlist_id (you can obtain it by copying the URI (i.e: spotify:user:user_id:playlist:playlist_id)).
+2.- Now, you have to create a playlist in Spotify with at least one track inside and then obtain their playlist_id (you can obtain it by copying the URI (i.e: spotify:user:user_id:playlist:playlist_id)).
 
 3.- Then using pip you have to install spotipy. Like this:
 
 "pip install spotipy"
 
-4.- For setting up spotipy you can do it using this https://spotipy.readthedocs.io/en/latest
+4.- For setting up spotipy you can do it using this https://spotipy.readthedocs.io/en/latest (just remember to set the enviromental variables)
 
 5.- If you have your app on spotify and you have installed spotipy succesfully now you only have to fill the information that 'test.py' needs.
 
 6.- Finally, after you fill it, you can create your random playlist by running:
 
-"python test.py user_id"
+"python test.py"
 
-**If it is the first time you run the script, it will ask you for a token (everything is explained in spotipy's documentation, so just check it out)**
+**If it is the first time you run the script, it will ask you for a token (just paste the url opened in your browser)**
 
-Just wait some minutes and that's all. Now you have your playlist ready !
+Just wait some minutes and that's it. Now you have your playlist ready !
--- test.py
+++ test.py
@@ -1,39 +1,38 @@
-#Libraries
-import sys
+# Libraries
 import os
-import spotipy
-import spotipy.util as util
+import pprint
 from random import randint
-#Enviromental variables
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Enviromental variables
 os.environ["SPOTIPY_CLIENT_ID"] = ""
 os.environ["SPOTIPY_CLIENT_SECRET"] = ""
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost/"
-#The scopes that we need for read and write.
+
 scope = 'user-library-read playlist-modify-public'
 
-#We receive username when running the script, also you can set it manually.
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print ("Usage: %s username") % (sys.argv[0],)
-    sys.exit()
+playlist_id = ''
 
-token = util.prompt_for_user_token(username, scope)
-tracks=[]
-if token:
-    sp = spotipy.Spotify(auth=token)
-    #The number in range, is the number of songs that you want in your playlist
-    for a in range(50):
-        #'library_size' represents the number of songs that you have in your library (not necesary the exact amount).
-        library_size=50
-        ran=randint(0,library_size)
-        results = sp.current_user_saved_tracks(limit=1, offset=ran)
-        for item in results['items']:
-            track = item['track']
-            tracks.append(track['id'])
-            #print (str(a+1)+".- "+track['id']+" "+str(track['name'].encode('ascii', 'ignore')) + ' - ' + str(track['artists'][0]['name'].encode('ascii', 'ignore')))
-    sp.trace = False
-    results = sp.user_playlist_replace_tracks(username, 'playlist_id', tracks)
-    print (results)
-else:
-    print ("Can't get token for", username)
+track_ids = []
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+# The number of tracks that you have in your library (not needed to be the exact amount)
+library_size = 9800
+
+# The number of tracks that you want in your playlist
+number_tracks_playlist = 50
+
+for i in range(number_tracks_playlist):
+
+    ran = randint(0, library_size)
+
+    result = sp.current_user_saved_tracks(limit=1, offset=ran)
+    for item in result['items']:
+        track = item['track']
+        track_ids.append(track['id'])
+
+results = sp.playlist_replace_items(playlist_id, track_ids)
+pprint.pprint(results)
